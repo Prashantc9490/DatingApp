5d4b7d4d96f4f1e0cfe5659405d2da11c0c84ccb
dhairyashil@live.com <dhairyashil@live.com>
2020-09-08 18:41:18 +0530

    Initial Commit

---
+++ WebAPI.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.API.Models;
+
+namespace WebAPI.API.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
