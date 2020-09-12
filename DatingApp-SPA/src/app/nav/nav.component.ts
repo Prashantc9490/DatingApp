fc49126a1d7f2c1c5da80d88dbff8eff68b2b44d
dhairyashil@live.com <dhairyashil@live.com>
2020-09-12 17:47:36 +0530

    Section 4

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged In Sucessfully');
+    }, error => {
+      console.log('Failed To Login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logout Sucessfully');
+  }
+}
--- WebAPI.API/Controllers/ValuesController.cs
+++ WebAPI.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace WEBAPI.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
